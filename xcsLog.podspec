6bf68df9ab858c782dd3932461bd097735f10468
Cypress <cypressmobileapp@gmail.com>
2018-12-09 13:50:59 +0800

    rename project

--- xcsLog.podspec
+++ xcsLog.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'xcsLog'
-  s.version          = '1.0.0.3'
+  s.version          = '1.0.0.5'
   s.summary          = 'A short description of xcsLog.'
 
 # This description is used to generate tags and improve search results.
---
+++ xcsLog/Classes/xcsLogM.framework/Headers/xcsLogM.h
@@ -0,0 +1,19 @@
+//
+//  xcsLogM.h
+//  xcsLogM
+//
+//  Created by Cypress on 2018/12/9.
+//  Copyright © 2018 xcs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for xcsLogM.
+FOUNDATION_EXPORT double xcsLogMVersionNumber;
+
+//! Project version string for xcsLogM.
+FOUNDATION_EXPORT const unsigned char xcsLogMVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <xcsLogM/PublicHeader.h>
+
+
